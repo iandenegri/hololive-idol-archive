f5e15f12e6d45a83861a291b30cc7b34ae2743cc
iandenegri <iandenegri@hotmail.com>
2020-07-31 15:45:29 -0400

    Loading optimization. Can cache static

--- holoidol_archive/settings.py
+++ holoidol_archive/settings.py
@@ -152,6 +152,7 @@ AWS_S3_HOST = 's3.us-east-2.amazonaws.com'
 AWS_DEFAULT_ACL = None
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+AWS_QUERYSTRING_AUTH = False
 
 ##########################
 # HEROKU
